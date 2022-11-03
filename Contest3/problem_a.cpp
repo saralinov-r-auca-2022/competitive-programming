fd96d069351ed413bd8f13d03a7acc25fb85d737
altcode <cosmosx1328@gmail.com>
2022-11-03 19:04:37 +0600

    :sparkles: Finish problem_a.cpp
    clang 8.0

---
+++ Contest3/problem_a.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+vector<int> prefixSums(vector<int> a) {
+
+    vector<int> p(a.size());
+    p[0] = a[0];
+    for (int i = 1; i < a.size(); i++) {
+        p[i] = p[i - 1] + a[i];
+    }
+    return p;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    vector<int> a(t);
+    for (int i = 0; i < t; i++) {
+        cin >> a[i];
+    }
+    auto s = prefixSums(a);
+    int q;
+    cin >> q;
+    int m, k;
+    while (q--) {
+        cin >> m;
+        cin >> k;
+        if (m == 0) {
+            cout << s[k] << "\n";
+        } else {
+            cout << s[k] - s[m - 1] << "\n";
+        }
+    }
+    return 0;
+}
