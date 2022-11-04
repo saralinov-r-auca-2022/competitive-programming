bf10d4b64c36308139dc6950456bbeed0e5d85a0
altcode <cosmosx1328@gmail.com>
2022-11-04 16:07:11 +0600

    :sparkles: Finish problem_d.cpp

---
+++ Contest1/problem_d.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    string a; cin >> a;
+    string b; cin >> b;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == b[i] && b[i] == '1') {
+            cout << "NO" << "\n";
+            return;
+        }
+    }
+    cout << "YES" << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+}
