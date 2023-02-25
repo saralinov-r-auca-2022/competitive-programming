47aac8a05a7a76ff60f1beae5e0360576ec66a41
altcode <cosmosx1328@gmail.com>
2023-02-26 00:42:12 +0600

    :art: Improve structure

--- Semester-1/Contest-1/Problem-A/solution.cpp
+++ Semester-1/Contest-1/Problem-A/solution.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+#ifndef INPUT_FILE
+#define INPUT_FILE "no"
+#endif
+
 void solve() {
     string s;
     cin >> s;
@@ -22,6 +26,8 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    string input_file = INPUT_FILE;
+    if (input_file != "no") freopen(INPUT_FILE, "r", stdin);
 
     int t;
     cin >> t;
--- Semester-1/Contest-1/Problem-B/solution.cpp
+++ Semester-1/Contest-1/Problem-B/solution.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+#ifndef INPUT_FILE
+#define INPUT_FILE "no"
+#endif
 
 void solve() {
     int l;
@@ -23,6 +26,9 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    string input_file = INPUT_FILE;
+    if (input_file != "no") freopen(INPUT_FILE, "r", stdin);
+
     int t;
     cin >> t;
     while (t--) {
