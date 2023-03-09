829c95b5ed06d4924d55a197a435a5a1e0cfa92f
altcode <cosmosx1328@gmail.com>
2023-03-09 23:32:06 +0600

    :sparkles: Finish Contest-2/Problem-E

---
+++ Semester-2/Contest-2/Problem-E/solution.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef INPUT_FILE
+#define INPUT_FILE "no"
+#endif
+
+void solve(int n) {
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    int money;
+    cin >> money;
+    sort(prices.begin(), prices.end());
+
+    int l = 0;
+    int r = n - 1;
+
+    int price1, price2;
+    while (l < r) {
+        if (prices[l] + prices[r] == money) {
+            price1 = prices[l];
+            price2 = prices[r];
+            l++;
+            r--;
+        } else if (prices[l] + prices[r] > money) {
+            r--;
+        } else {
+            l++;
+        }
+    }
+
+    cout << "Peter should buy books whose prices are " << price1 << " and " << price2 << ".\n";
+    cout << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string input_file = INPUT_FILE;
+    if (input_file != "no") freopen(INPUT_FILE, "r", stdin);
+
+    int n;
+    while (cin >> n) {
+        solve(n);
+    }
+    return 0;
+}
