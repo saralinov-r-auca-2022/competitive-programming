5a63df6cebb9d5a24b6cb2c65ff4d119923e98ae
altcode <cosmosx1328@gmail.com>
2023-03-09 19:58:29 +0600

    :truck: Move Contest-5/Problem-A to Contest-2/Problem-F

--- Semester-2/Contest-5/Problem-A/solution.cpp
+++ Semester-2/Contest-5/Problem-A/solution.cpp
@@ -7,32 +7,7 @@ using namespace std;
 #endif
 
 void solve() {
-    int n; cin >> n;
-    vector<int> sequence(n);
 
-    for (int i = 0; i < n; i++) {
-        cin >> sequence[i];
-    }
-
-    int max_vertex = 0;
-    int l = 0;
-    int r = n - 1;
-
-    while (l != r) {
-        if (sequence[l] != max_vertex + 1) {
-            l++;
-        } else if (sequence[r] != max_vertex + 1) {
-            r--;
-        } else if (sequence[r] == sequence[l]) {
-            max_vertex++;
-        }
-    }
-
-    if (sequence[l] == max_vertex + 1) {
-        max_vertex++;
-    }
-
-    cout << max_vertex << "\n";
 }
 
 int main() {
@@ -41,8 +16,9 @@ int main() {
     string input_file = INPUT_FILE;
     if (input_file != "no") freopen(INPUT_FILE, "r", stdin);
 
-    int t; cin >> t;
-    while(t--) {
+    int t;
+    cin >> t;
+    while (t--) {
         solve();
     }
     return 0;
