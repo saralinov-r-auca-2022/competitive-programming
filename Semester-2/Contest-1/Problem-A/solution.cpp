fc0190af6f9a9cd44bbc9f298f3bea323757b96d
altcode <cosmosx1328@gmail.com>
2023-09-30 22:05:17 +0600

    :sparkles: Solve 1-A

--- Semester-2/Contest-1/Problem-A/solution.cpp
+++ Semester-2/Contest-1/Problem-A/solution.cpp
@@ -7,7 +7,25 @@ using namespace std;
 #endif
 
 void solve() {
+    int n; cin >> n;
+    int counter = 0;
+    int i = 0;
 
+    if (n == 2 || n == 3) {
+        cout << 0;
+        return;
+    }
+
+    for (int j = 2; j <= sqrt(n); j++) {
+        if (n % j == 0) {
+            counter += 2;
+        }
+        i = j;
+    }
+    if ((int) (n / i) == sqrt(n)) {
+        counter--;
+    }
+    cout << counter;
 }
 
 int main() {
