4fe068699ac6b7b94752ddea124e5976f9da092a
altcode <cosmosx1328@gmail.com>
2023-10-01 17:12:15 +0600

    :sparkles: Solve 2-C

--- Semester-2/Contest-2/Problem-C/solution.cpp
+++ Semester-2/Contest-2/Problem-C/solution.cpp
@@ -6,11 +6,38 @@ using namespace std;
 #define INPUT_FILE "no"
 #endif
 
-void solve() {
+#define int long long
 
+void solve() {
+    int size; cin >> size;
+    int sum; cin >> sum;
+
+    vector<int> arr(size);
+
+    for (int &i: arr) {
+        cin >> i;
+    }
+
+    int l = 0;
+    int r = size - 1;
+
+    while (l < r) {
+        if (arr[l] + arr[r] == sum) {
+            cout << "YES" << "\n";
+            return;
+        }
+
+        if (arr[l] + arr[r] < sum) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+
+    cout << "NO" << "\n";
 }
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string input_file = INPUT_FILE;
