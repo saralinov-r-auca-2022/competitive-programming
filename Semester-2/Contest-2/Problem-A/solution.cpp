a0751267b58135793a205efb0871ed302112d247
altcode <cosmosx1328@gmail.com>
2023-10-01 16:30:09 +0600

    :sparkles: Solve 2-A

--- Semester-2/Contest-2/Problem-A/solution.cpp
+++ Semester-2/Contest-2/Problem-A/solution.cpp
@@ -6,11 +6,46 @@ using namespace std;
 #define INPUT_FILE "no"
 #endif
 
-void solve() {
+#define int long long
 
+void solve() {
+    int n, m;
+    cin >> n;
+
+    vector<int> firstArr(n);
+    vector<int> result;
+
+    for (int i = 0; i < n; i++) {
+        cin >> firstArr[i];
+    }
+    firstArr.push_back(INT_MAX);
+
+    cin >> m;
+    vector<int> secondArr(m);
+    for (int i = 0; i < m; i++) {
+        cin >> secondArr[i];
+    }
+    secondArr.push_back(INT_MAX);
+
+    int i = 0;
+    int j = 0;
+
+    while (i < n || j < m) {
+        if (firstArr[i] < secondArr[j]) {
+            result.push_back(firstArr[i]);
+            i++;
+        } else {
+            result.push_back(secondArr[j]);
+            j++;
+        }
+    }
+
+    for (i = 0; i < n+m; i++) {
+        cout << result[i] << " ";
+    }
 }
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string input_file = INPUT_FILE;
@@ -18,8 +53,10 @@ int main() {
 
     int t;
     cin >> t;
+
     while (t--) {
         solve();
     }
+
     return 0;
 }
