b1bd18c41060fa235511b66e6384626df1f58559
altcode <cosmosx1328@gmail.com>
2023-10-08 18:48:36 +0600

    :sparkles: Solve 2-J

--- Semester-2/Contest-2/Problem-J/solution.cpp
+++ Semester-2/Contest-2/Problem-J/solution.cpp
@@ -6,11 +6,38 @@ using namespace std;
 #define INPUT_FILE "no"
 #endif
 
-void solve() {
+#define int long long
 
+void solve() {
+    int p, m;
+    cin >> p >> m;
+    vector<int> planets;
+
+    for (int i = 0; i < p; i++) {
+        int a;
+        cin >> a;
+        planets.push_back(a);
+    }
+
+    int l = 0, r = 0, max = 0, sum = 0, min_sum = 0;
+    for (; r < p; r++) {
+        sum += planets[r];
+
+        while (sum > m) {
+            sum -= planets[l];
+            l++;
+        }
+
+        if (r - l + 1 > max || r - l + 1 == max && min_sum > sum) {
+            max = r - l + 1;
+            min_sum = sum;
+        }
+    }
+
+    cout << min_sum << " " << max << "\n";
 }
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string input_file = INPUT_FILE;
