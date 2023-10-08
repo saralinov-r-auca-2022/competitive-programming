e6b8738f5cb75e1b15a283da7584dc691963df7f
altcode <cosmosx1328@gmail.com>
2023-10-08 17:22:52 +0600

    :sparkles: Solve 2-I

--- Semester-2/Contest-2/Problem-I/solution.cpp
+++ Semester-2/Contest-2/Problem-I/solution.cpp
@@ -6,11 +6,45 @@ using namespace std;
 #define INPUT_FILE "no"
 #endif
 
-void solve() {
+#define int long long
 
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    vector<int> stations;
+
+    for (int i = 0; i < a; i++) {
+        int s;
+        cin >> s;
+        stations.push_back(s);
+    }
+
+    int max = 0, number_of_people = 0, sum = stations[0];
+
+    for (int l = 0, r = 0; r < a;) {
+        if (sum > b) {
+            sum -= stations[l];
+            if (l == r) {
+                r++;
+                sum += stations[r];
+            }
+            l++;
+        } else {
+            if (r - l + 1 > max) {
+                max = r - l + 1;
+                number_of_people = sum;
+            } else if (r - l + 1 == max) {
+                number_of_people = min(sum, number_of_people);
+            }
+            r++;
+            sum += stations[r];
+        }
+    }
+
+    cout << number_of_people << " " << max << "\n";
 }
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string input_file = INPUT_FILE;
@@ -21,5 +55,6 @@ int main() {
     while (t--) {
         solve();
     }
+
     return 0;
 }
